cbde62df636e3c3c1e4d6ca234244006bc435912
Mohammad Seydur Rahman <seydur.rahman82@gmail.com>
2025-01-18 20:10:23 +0600

    dashboard sacffholding

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@material-tailwind/react";
+
+function App() {
+  return (
+    <div className="bg-red-300">
+      <Button>Seydur</Button>
+    </div>
+  );
+}
+
+export default App;
